bf4263f23ff62c02a29d393d22e18b17b794cf5a
starshiner <1642965215@qq.com>
2015-08-02 10:38:28 +0800

    modi code by starshiner --2

--- game2/js/main.js
+++ game2/js/main.js
@@ -1,8 +1,11 @@
-var gameWidth = 360,gameHeight = 640;//定义游戏主尺寸
+var gameWidth = 360,gameHeight = 505;//定义游戏主尺寸
 var musicSwitch = 1;//音乐开关
+var score = "0000000000";
+var ftStyle = { font: "40px Arial", fill: "#000000" };
+var betadirection=0,gammadirection=0;
 var game = new Phaser.Game(gameWidth,gameHeight,Phaser.AUTO,'canvas');//实例化game
 game.States = {};//存放状态
-
+	
 game.States.boot = function(){ //移动设备适应
 	this.preload = function(){
 		if(!game.device.desktop){
@@ -36,7 +39,8 @@ game.States.preload = function(){//加载游戏资源
     	game.load.image('pause','assets/pause.png'); //游戏暂停按钮
     	game.load.image('background','assets/bgpic.png'); //游戏背景
     	game.load.image('title','assets/title.png'); //游戏背景
-    	game.load.spritesheet('player','assets/player.png',97,123,2); //飞机
+    	game.load.image('bullet','assets/zd2.png'); //游戏背景
+    	game.load.spritesheet('player','assets/player.png',70,89,2); //飞机
     	// game.load.audio('hit_ground_sound', 'assets/ouch.wav'); //撞击地面的音效
 	}
 	this.create = function(){
@@ -47,13 +51,13 @@ game.States.preload = function(){//加载游戏资源
 game.States.menu = function(){//显示开始菜单
 	this.create = function(){
 		game.add.tileSprite(0,0,game.width,game.height,'background'); //背景图
-		this.title = game.add.sprite(80,150,'title'); //标题
+		this.title = game.add.sprite(80,game.height*0.4/2,'title'); //标题
 		var btn = game.add.button(game.width/2,game.height/2,'startGame',function(){//开始按钮
 			game.state.start('play');
 		});
 		btn.anchor.setTo(0.5,0.5);
 		var btn = game.add.button(game.width/2,game.height*1.2/2,'rank',function(){//开始按钮
-			this.rank = game.add.sprite(52,100,'listRank'); //排行榜
+			this.rank = game.add.sprite(52,game.height*0.2/2,'listRank'); //排行榜
 		});
 		btn.anchor.setTo(0.5,0.5);
 	}
@@ -62,11 +66,57 @@ game.States.menu = function(){//显示开始菜单
 game.States.play = function(){
 	this.create = function(){
 		game.add.tileSprite(0,0,game.width,game.height,'background').autoScroll(0,20); //背景图
-		this.player = game.add.sprite(0,517,'player'); //添加飞机
+		this.player = game.add.sprite(0,game.height-89,'player'); //添加飞机
+		game.physics.enable(this.player, Phaser.Physics.ARCADE);
 		// game.physics.enable([this.player], Phaser.Physics.ARCADE);//禁止飞机飞出界外
 		// this.player.body.collideWorldBounds = true;
 		this.player.inputEnabled = true;
 		this.player.input.enableDrag();
+		this.player.enableBody = true;
+		game.physics.arcade.enable(this.player);
+		this.player.animations.add('fly', [1,2], 10, true);
+		this.bullets = game.add.group();
+	    this.bullets.enableBody = true;
+	    this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+
+	    this.bullets.createMultiple(50, 'bullet');
+	    this.bullets.setAll('checkWorldBounds', true);
+	    this.bullets.setAll('outOfBoundsKill', true);
+	    this.timer = game.time.events.loop(300,this.add_two_bullet,this);
+
+	    this.label_score = game.add.text(100 , 5 , score, ftStyle);
+
+	    var pauseBtn = game.add.button(0,0,'pause',function(){//暂停按钮
+			game.paused = true;
+		});
+
+		window.addEventListener("deviceorientation", this.deviceOrientationListener);
+	}
+	this.update = function(){
+		this.player.animations.play('fly');
+	}
+	this.fly = function(){
+
+	}
+	this.add_one_bullet = function(){   //发射单炮
+		this.bullet = this.bullets.getFirstDead();
+  	 	this.bullet.reset(this.player.x + 33, this.player.y );
+    	this.bullet.body.velocity.y = -500;
+	}
+	this.add_two_bullet = function(){   //发射双炮
+		this.bullet = this.bullets.getFirstDead();
+  	 	this.bullet.reset(this.player.x + 33 - 10, this.player.y );
+    	this.bullet.body.velocity.y = -500;
+
+    	this.bullet = this.bullets.getFirstDead();
+  	 	this.bullet.reset(this.player.x + 33 + 10, this.player.y );
+    	this.bullet.body.velocity.y = -500;
+	}
+	this.deviceOrientationListener = function(event) {
+	  	betadirection = Math.round(event.beta);//负前正后
+		gammadirection = Math.round(event.gamma);//负左正右
+		// this.player.body.velocity.x = gammadirection*10;
+		// this.player.body.velocity.y = betadirection*10;
 	}
 }
 
